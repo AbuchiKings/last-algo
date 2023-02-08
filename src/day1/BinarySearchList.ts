7df839131e08997a2d1dd4a786d9e292303de0c6
abuchikings <abuchikingsley76@gmail.com>
2023-02-08 11:30:34 +0100

    Binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
+    do {
+        const midpoint = Math.floor(low + (high - low) / 2 );
+        const present_value = haystack[midpoint];
 
+        if (present_value === needle) {
+            return true;
+        } else if (present_value > needle) {
+            high = midpoint;
+        } else if (present_value < needle) {
+            low = midpoint + 1;
+        }
+    } while (low < high);
+    return false;
 }
